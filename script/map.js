c9860065d618ea3919e9e5b78ac18111b4227a97
jimmywilson11 <jimmy.wilson11@gmail.com>
2019-12-15 13:29:45 +0200

    UPD: map.js

--- script/map.js
+++ script/map.js
@@ -1,8 +1,8 @@
 (function () {
-            var setting = {"height":647,"width":1920,"zoom":15,"queryString":"ул. Горького, 60, Черкассы, Черкасская область, Украина","place_id":"EmHRg9C7LiDQk9C-0YDRjNC60L7Qs9C-LCA2MCwg0KfQtdGA0LrQsNGB0YHRiywg0KfQtdGA0LrQsNGB0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCj0LrRgNCw0LjQvdCwIjASLgoUChIJVenL4qdL0UAR35l8ffIfgyQQPCoUChIJfSwH4adL0UARI3siqB7sRyY","satellite":false,"centerCoord":[49.42597066854488,32.09476404999998],"cid":"0xe1612c7328a3d9ab","lang":"ru","cityUrl":"/ukraine/cherkasy-35348","id":"map-9cd199b9cc5410cd3b1ad21cab2e54d3","embed_id":"131150"};
+            var setting = {"height":647,"width":1920,"zoom":16,"queryString":"вулиця Горького, 60, Cherkasy, Cherkasy Oblast, Ukraine","place_id":"EkXQstGD0LvQuNGG0Y8g0JPQvtGA0YzQutC-0LPQviwgNjAsIENoZXJrYXN5LCBDaGVya2FzeSBPYmxhc3QsIFVrcmFpbmUiMBIuChQKEglV6cvip0vRQBHfmXx98h-DJBA8KhQKEgl9LAfhp0vRQBEjeyKoHuxHJg","satellite":false,"centerCoord":[49.42597066854488,32.09476404999998],"cid":"0xe1612c7328a3d9ab","lang":"en","cityUrl":"/ukraine/cherkasy-35348","id":"map-9cd199b9cc5410cd3b1ad21cab2e54d3","embed_id":"131152"};
             var d = document;
             var s = d.createElement('script');
-            s.src = 'https://1map.com/js/script-for-user.js?embed_id=131150';
+            s.src = 'https://1map.com/js/script-for-user.js?embed_id=131152';
             s.async = true;
             s.onload = function (e) {
               window.OneMap.initMap(setting)
